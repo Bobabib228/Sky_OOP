031a315f7f07153b02869c9ed8379fdfac732315
Bobabib228 <egor.tchichi@yandex.ru>
2026-09-03 19:44:28 +0300

    saving

--- src/category_class.py
+++ src/category_class.py
@@ -4,7 +4,7 @@ from product_class import Product
 class Category:
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
 
     category_count = 0
     product_count = 0
@@ -12,7 +12,16 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    @property
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @add_product.getter
+    def add_product(self):
+        return self.__products
