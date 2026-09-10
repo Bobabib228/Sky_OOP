f8261203bb8e2f1ad054dcbfae2453b7b49823da
Bobabib228 <egor.tchichi@yandex.ru>
2026-09-10 20:09:34 +0300

    modification class Category

--- src/category_class.py
+++ src/category_class.py
@@ -21,11 +21,15 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products)
 
-    def __str__(self):
+    @property
+    def products(self):
         lines = []
         for p in self.__products:
             lines.append(f"{p.name}, Цена: {p.product_price} руб., Остаток: {p.quantity} шт.")
         return "\n".join(lines)
+
+    def __str__(self):
+        return f"{self.name}, количество продуктов: {Category.product_count} шт."
 
 
 
--- tests/test_category_class.py
+++ tests/test_category_class.py
@@ -13,6 +13,6 @@ def test_category(category, product):
     assert Category.category_count == 1
     assert Category.product_count == 1
     category_str = str(category)
-    assert category_str == 'Samsung Galaxy S23 Ultra, Цена: 18000.0 руб., Остаток: 5 шт.'
+    assert category_str == 'Смартфон, количество продуктов: 1 шт.'
 
 
